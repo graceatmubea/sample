9d5b3a07fdedc194e8399cc37f0d2d65e24a4169
graceatmubea <grace.chan@mubea.com>
2025-07-03 12:54:29 +0000

    ll# Please enter the commit message for your changes. Lines starting

--- pgTitle.py
+++ pgTitle.py
@@ -11,6 +11,6 @@ st.title("CI-QUALITY WEEK GAMES")
 if st.button(label="Jeopardy", key=None, help=None, type="primary", icon=None,
             disabled=False, use_container_width=True):
         st.switch_page("pg2.py")
-if st.button(label="Root Cause Analysis", key=None, help=None, type="primary", icon=None,
-            disabled=False, use_container_width=True):
-        st.switch_page("pg3.py")
+#if st.button(label="Root Cause Analysis", key=None, help=None, type="primary", icon=None,
+ #           disabled=False, use_container_width=True):
+  #      st.switch_page("pg3.py")
